f1917c33a6cb59e209f563485708e3e06d674c8f
Maxence <maxgrymonprez@live.fr>
2021-12-11 14:38:49 +0000

    Add Spring Boot pre-initialized project files 🍃

---
+++ src/main/java/org/colosseumproject/ludus/LudusApplication.java
@@ -0,0 +1,13 @@
+package org.colosseumproject.ludus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudusApplication.class, args);
+	}
+
+}
