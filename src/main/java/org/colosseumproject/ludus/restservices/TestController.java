941dfef61a171ad46c78950ed6baa0a579c65233
Maxence <maxgrymonprez@live.fr>
2021-12-12 17:29:53 +0000

    Add Test classes

---
+++ src/main/java/org/colosseumproject/ludus/restservices/TestController.java
@@ -0,0 +1,20 @@
+package org.colosseumproject.ludus.restservices;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/test")
+	public Test test(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Test(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
