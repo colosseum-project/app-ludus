08593c29672d5151909c2324f56b431c53425280
Maxence <maxgrymonprez@live.fr>
2022-03-21 00:17:03 +0000

    Remove unnecessary imports

--- src/test/java/org/colosseumproject/ludus/integration/DuelControllerTest.java
+++ src/test/java/org/colosseumproject/ludus/integration/DuelControllerTest.java
@@ -17,8 +17,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
