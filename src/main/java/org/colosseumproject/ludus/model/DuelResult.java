73c3de323fe03a300db82bcc0c62b9bf1a13d09d
VouDoo <maxgrymonprez@live.fr>
2023-10-05 15:28:27 +0200

    Upgrade Gradle wrapper and SpringFramework Boot

--- src/main/java/org/colosseumproject/ludus/model/DuelResult.java
+++ src/main/java/org/colosseumproject/ludus/model/DuelResult.java
@@ -3,15 +3,15 @@ package org.colosseumproject.ludus.model;
 import java.sql.Timestamp;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OrderColumn;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OrderColumn;
+import jakarta.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
